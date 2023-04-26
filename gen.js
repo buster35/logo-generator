3f875674f76a143a45f06cc2ed95b0ed8671f323
Sleepy Cat <gilb2966@gmail.com>
2023-04-26 17:56:39 -0500

    continued to build shape class and subclasses

--- gen.js
+++ gen.js
@@ -1,6 +1,5 @@
 const fs = require("fs")
 const writeToSVG = require("./index.js")
-const path = require("path")
 const fileName = fs.writeFile(`./examples/${response.logoCharacters}.SVG`)
 
 
@@ -8,7 +7,8 @@ function generateLogo(response) {
 
 
 
-
+  // <?xml version="1.0" standalone="no"?>
+  // <svg width="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg">
 
 
 }
